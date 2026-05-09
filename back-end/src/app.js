50da9216800b4fc1026c27fc8e8b4cc1cb2b52ec
mofalqueto <monicafalqueto1985@gmail.com>
2026-05-09 18:33:13 -0300

    (09/05) Armazenamento do token JWT em cookie HTTP-Only e seguro

---
+++ back-end/src/app.js
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv'
+dotenv.config() // Carrega as variáveis de ambiente do arquivo .env
+
+import express, { json, urlencoded } from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+
+const app = express()
+
+import cors from 'cors'
+
+
+app.use(cors({
+ origin: process.env.ALLOWED_ORIGINS.split(','),
+  credentials: true
+}))
+
+
+app.use(logger('dev'))
+app.use(json())
+app.use(urlencoded({ extended: false }))
+app.use(cookieParser())
+
+/*********** ROTAS DA API **************/
+
+import carsRouter from './routes/cars.js'
+app.use('/cars', carsRouter)
+
+import customersRouter from './routes/customers.js'
+app.use('/customers', customersRouter)
+
+import usersRouter from './routes/users.js'
+app.use('/users', usersRouter)
+
+export default app
